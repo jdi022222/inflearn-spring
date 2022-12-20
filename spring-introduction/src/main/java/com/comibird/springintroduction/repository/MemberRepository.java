f964b869438ace3e0328fc2a8a4600e32781c103
jdi022222 <jdi022222@gmail.com>
2022-12-20 21:13:44 +0900

    feat: Member entity, MemberRepository 인터페이스 및 구현체 생성

---
+++ spring-introduction/src/main/java/com/comibird/springintroduction/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.comibird.springintroduction.repository;
+
+import com.comibird.springintroduction.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+  Member save(Member member);
+
+  Optional<Member> findById(Long id);
+
+  Optional<Member> findByName(String name);
+
+  List<Member> findAll();
+}
