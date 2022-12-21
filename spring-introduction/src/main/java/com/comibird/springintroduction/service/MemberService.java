12f668e6b93bc721d4f9b0ef29260b133f4eaeb6
jdi022222 <jdi022222@gmail.com>
2022-12-22 01:20:38 +0900

    feat: MemberService test case 작성

--- spring-introduction/src/main/java/com/comibird/springintroduction/service/MemberService.java
+++ spring-introduction/src/main/java/com/comibird/springintroduction/service/MemberService.java
@@ -23,13 +23,9 @@ public class MemberService {
 
   private void validateDuplicateMember(Member member) {
     memberRepository.findByName(member.getName())
-            .ifPresent(m -> {
-              try {
-                throw new IllegalAccessException("이미 존재하는 회원입니다.");
-              } catch (IllegalAccessException e) {
-                e.printStackTrace();
-              }
-            });
+        .ifPresent(m -> {
+          throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
   }
 
   /**
