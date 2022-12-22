2d26cebfe25a4f99e41713118fb76c2053ff76c6
jdi022222 <jdi022222@gmail.com>
2022-12-22 14:06:53 +0900

    feat: 회원 등록 구현

--- spring-introduction/src/main/java/com/comibird/springintroduction/controller/MemberController.java
+++ spring-introduction/src/main/java/com/comibird/springintroduction/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.comibird.springintroduction.controller;
 
+import com.comibird.springintroduction.domain.Member;
+import com.comibird.springintroduction.form.MemberForm;
 import com.comibird.springintroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +18,19 @@ public class MemberController {
     this.memberService = memberService;
   }
 
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String createForm(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
+  }
 }
 
