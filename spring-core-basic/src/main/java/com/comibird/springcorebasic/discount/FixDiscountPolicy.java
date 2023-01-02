7edf9afc94f64f2c3afd843426cc806b156a5e2c
jdi022222 <jdi022222@gmail.com>
2023-01-02 14:34:01 +0900

    feat: add discountPolicy

---
+++ spring-core-basic/src/main/java/com/comibird/springcorebasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package com.comibird.springcorebasic.discount;
+
+import com.comibird.springcorebasic.member.Grade;
+import com.comibird.springcorebasic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+  private int discountFixAmount = 1000; // 고정으로 1000원 할인
+
+  @Override
+  public int discount(Member member, int price) {
+    if (member.getGrade() == Grade.Vip) {
+      return discountFixAmount;
+    }
+    return 0;
+  }
+}
