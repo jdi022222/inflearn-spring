21343697a39297cba0d91bdb121804db017e0bed
jdi022222 <jdi022222@gmail.com>
2023-01-03 02:32:58 +0900

    refactor: AppConfig 리팩토링

--- spring-core-basic/src/main/java/com/comibird/springcorebasic/AppConfig.java
+++ spring-core-basic/src/main/java/com/comibird/springcorebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package com.comibird.springcorebasic;
 
+import com.comibird.springcorebasic.discount.DiscountPolicy;
 import com.comibird.springcorebasic.discount.FixDiscountPolicy;
+import com.comibird.springcorebasic.member.MemberRepository;
 import com.comibird.springcorebasic.member.MemberService;
 import com.comibird.springcorebasic.member.MemberServiceImpl;
 import com.comibird.springcorebasic.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.comibird.springcorebasic.order.OrderServiceImpl;
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  private DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }
