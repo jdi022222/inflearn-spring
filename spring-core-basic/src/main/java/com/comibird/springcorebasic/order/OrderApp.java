bdb6b42eead6dedb809b107ce98d60378a921e94
jdi022222 <jdi022222@gmail.com>
2023-01-04 22:33:22 +0900

    feat: 스프링 컨테이너 적용, getBean을 통해 빈 찾도록 변경

--- spring-core-basic/src/main/java/com/comibird/springcorebasic/order/OrderApp.java
+++ spring-core-basic/src/main/java/com/comibird/springcorebasic/order/OrderApp.java
@@ -4,13 +4,18 @@ import com.comibird.springcorebasic.AppConfig;
 import com.comibird.springcorebasic.member.Grade;
 import com.comibird.springcorebasic.member.Member;
 import com.comibird.springcorebasic.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
   public static void main(String[] args) {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
+//    AppConfig appConfig = new AppConfig();
+//    MemberService memberService = appConfig.memberService();
+//    OrderService orderService = appConfig.orderService();
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+    OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
     Long memberId = 1L;
     Member member = new Member(memberId, "memberA", Grade.Vip);
