2440263dc7a6ae198f49efc781ea2e46a031535e
jdi022222 <jdi022222@gmail.com>
2023-02-13 17:36:58 +0900

    feat: 빈을 list와 map으로 가져와서 전략 패턴 적용

--- spring-core-basic/src/main/java/com/comibird/springcorebasic/discount/FixDiscountPolicy.java
+++ spring-core-basic/src/main/java/com/comibird/springcorebasic/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.comibird.springcorebasic.discount;
 
 import com.comibird.springcorebasic.member.Grade;
 import com.comibird.springcorebasic.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
   private int discountFixAmount = 1000; // 고정으로 1000원 할인
