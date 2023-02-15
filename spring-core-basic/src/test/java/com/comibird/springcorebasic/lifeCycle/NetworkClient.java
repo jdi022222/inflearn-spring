cf6a0793aa0a8b16b9a73cb05c5271b20691c85d
jdi022222 <jdi022222@gmail.com>
2023-02-16 00:32:11 +0900

    feat: 방법3 : 어노테이션을 이용한 빈 생명주기 콜백

--- spring-core-basic/src/test/java/com/comibird/springcorebasic/lifeCycle/NetworkClient.java
+++ spring-core-basic/src/test/java/com/comibird/springcorebasic/lifeCycle/NetworkClient.java
@@ -1,9 +1,9 @@
 package com.comibird.springcorebasic.lifeCycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class NetworkClient{
+public class NetworkClient {
 
   private String url;
 
@@ -29,12 +29,14 @@ public class NetworkClient{
     System.out.println("close: " + url);
   }
 
+  @PostConstruct
   public void init() throws Exception {
     System.out.println("NetworkClient.afterPropertiesSet");
     connect();
     call("초기화 연결 메시지");
   }
 
+  @PreDestroy
   public void close() throws Exception {
     System.out.println("NetworkClient.destroy");
     disconnect();
