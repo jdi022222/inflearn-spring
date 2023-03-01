a0aa9fb0241e1485ba1d16ec33214b38536c0584
jdi022222 <jdi022222@gmail.com>
2023-03-01 23:22:10 +0900

    feat: JSR-330 Provider의 DL을 이용해 지정한 빈을 컨테이너에서 찾기

--- spring-core-basic/src/test/java/com/comibird/springcorebasic/scope/SingletonWithPrototypeTest1.java
+++ spring-core-basic/src/test/java/com/comibird/springcorebasic/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,10 +46,10 @@ public class SingletonWithPrototypeTest1 {
   static class ClintBean {
 
     @Autowired
-    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+    private Provider<PrototypeBean> provider;
 
     public int logic() {
-      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+      PrototypeBean prototypeBean = provider.get();
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
