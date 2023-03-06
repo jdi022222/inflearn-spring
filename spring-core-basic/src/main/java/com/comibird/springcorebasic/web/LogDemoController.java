3e496603a85bfda54481ffbad747349e6c7d8442
jdi022222 <jdi022222@gmail.com>
2023-03-06 09:41:23 +0900

    feat: request 스코프 생성, 오류 발생

---
+++ spring-core-basic/src/main/java/com/comibird/springcorebasic/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package com.comibird.springcorebasic.web;
+
+import com.comibird.springcorebasic.common.MyLogger;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+  private final LogDemoService logDemoService;
+  private final MyLogger myLogger;
+
+  @RequestMapping("log-demo")
+  @ResponseBody
+  public String logDemo(HttpServletRequest request) {
+    String requestURL = request.getRequestURL().toString();
+    myLogger.setRequestURL(requestURL);
+
+    myLogger.log("controller test");
+    logDemoService.logic("testId");
+
+    return "OK";
+  }
+}
