87504e2719332f8d94b99b672ce98b3746758477
jdi022222 <jdi022222@gmail.com>
2023-04-05 22:03:04 +0900

    feat: 회원 관리 웹 애플리케이션 요구사항

---
+++ spring-mvc-1/servlet/src/test/java/com/example/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("멤버 저장")
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    @DisplayName("멤버 전체 조회")
+    void findAll() {
+        //given
+        Member member1 = new Member("hello1", 20);
+        Member member2 = new Member("hello2", 21);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result).hasSize(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
