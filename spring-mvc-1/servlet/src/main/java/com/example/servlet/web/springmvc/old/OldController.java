2627994a1a5c6791d4dd31bf40489bde154d84f5
jdi022222 <jdi022222@gmail.com>
2023-04-07 10:03:08 +0900

    feat: 핸들러 매핑과 핸들러 어댑터

---
+++ spring-mvc-1/servlet/src/main/java/com/example/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package com.example.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+// 핸들러 매핑에서 해당 컴포넌트를 찾음 -> 링크로 가면 출력 됨
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
