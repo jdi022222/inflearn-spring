a6f5bb7402df890f9b61983c0b5f796e5f8a3876
jdi022222 <jdi022222@gmail.com>
2023-04-11 00:03:07 +0900

    feat: 상품 등록 처리 - @ModelAttribute

--- spring-mvc-1/springmvc-item-service/item-service/src/main/java/com/comibird/itemservice/domin/item/Item.java
+++ spring-mvc-1/springmvc-item-service/item-service/src/main/java/com/comibird/itemservice/domin/item/Item.java
@@ -8,10 +8,14 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+    public Item() {
+
+    }
 
     public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
     }
+
 }
