809d8ce2356ba08692dc6248efa9e9200c790c29
jdi022222 <jdi022222@gmail.com>
2023-04-17 11:10:34 +0900

    feat: JDBC 개발 - 등록

---
+++ spring-db-1/jdbc/src/test/java/com/comibird/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,19 @@
+package com.comibird.jdbc.repository;
+
+import com.comibird.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+    }
+
+}
