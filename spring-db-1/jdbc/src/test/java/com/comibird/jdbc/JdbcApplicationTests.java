28c573135e8930b0a303c0a962fec9cb187a198e
jdi022222 <jdi022222@gmail.com>
2023-04-17 10:14:15 +0900

    chore: init 스프링 DB 1편 - 데이터 접근 핵심 원리

---
+++ spring-db-1/jdbc/src/test/java/com/comibird/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comibird.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
