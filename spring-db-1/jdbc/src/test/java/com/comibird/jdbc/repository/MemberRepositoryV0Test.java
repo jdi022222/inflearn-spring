6b535c9f1fd50dd7d17206bd025d8e4244f8b52b
jdi022222 <jdi022222@gmail.com>
2023-04-17 11:16:42 +0900

    feat: JDBC 개발 - 조회

--- spring-db-1/jdbc/src/test/java/com/comibird/jdbc/repository/MemberRepositoryV0Test.java
+++ spring-db-1/jdbc/src/test/java/com/comibird/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,15 @@
 package com.comibird.jdbc.repository;
 
 import com.comibird.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-class MemberRepositoryV0Test {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Slf4j
+class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
@@ -14,6 +17,10 @@ class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
-    }
 
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+    }
 }
