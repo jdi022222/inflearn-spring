7f88331e0db8caada6c8d3f97e7e05df74612e02
jdi022222 <jdi022222@gmail.com>
2023-04-17 12:29:01 +0900

    feat: DataSource 적용

--- spring-db-1/jdbc/sql/scheme.sql
+++ spring-db-1/jdbc/sql/scheme.sql
@@ -6,3 +6,5 @@ create table member (
 );
 insert into member(member_id, money) values ('hi1',10000);
 insert into member(member_id, money) values ('hi2',20000);
+
+
---
+++ spring-db-1/jdbc/src/test/java/com/comibird/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,53 @@
+package com.comibird.jdbc.repository;
+
+import com.comibird.jdbc.domain.Member;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static com.comibird.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        //기본 DriverManager - 항상 새로운 커넥션 획득
+        //DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        //커넥션 풀링: HikariProxyConnection -> JdbcConnection
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException, InterruptedException {
+        log.info("start");
+
+        //save
+        Member member = new Member("memberV0", 10000);
+        repository.save(member);
+
+        //findById
+        Member memberById = repository.findById(member.getMemberId());
+        assertThat(memberById).isNotNull();
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+    }
+}
