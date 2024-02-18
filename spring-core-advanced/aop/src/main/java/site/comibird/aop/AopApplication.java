573758b1346ff76c5968e1ecbff6ebc4d4fe6f5a
jdi022222 <jdi022222@gmail.com>
2024-02-18 13:47:02 +0900

    feat: 스프링 AOP 구현4 - 포인트컷 참조

--- spring-core-advanced/aop/src/main/java/site/comibird/aop/AopApplication.java
+++ spring-core-advanced/aop/src/main/java/site/comibird/aop/AopApplication.java
@@ -7,9 +7,10 @@ import org.springframework.context.annotation.Import;
 import site.comibird.aop.order.aop.AspectV1;
 import site.comibird.aop.order.aop.AspectV2;
 import site.comibird.aop.order.aop.AspectV3;
+import site.comibird.aop.order.aop.AspectV4Pointcut;
 
 @SpringBootApplication
-@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
